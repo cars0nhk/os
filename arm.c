cf7105302f79c40184d9ce9e018aa706b18a509d
Carson <ayokaman@gmail.com>
2015-04-03 13:53:59 +0800

    2command now, printhihi and printbyebye

--- arm.c
+++ arm.c
@@ -5,133 +5,67 @@
 
 int main(int argc, char *argv[])
 {
-    char  buf[80], buf2[80];
-
-    int handcard=atoi(argv[2]);
-    char charArray[104];
-    char card[52][2];
-    int i,j;
-    for (i = 0; i < 104; i++)
-    {
-        scanf( "%1s", &charArray[i]);
-    }
-    j=0;
-    for (i = 0; i < 104;i++)
-    {
-        card[j][0]=charArray[i++];
-        card[j][1]=charArray[i];
-        j++;
-    }
-    int num_child = atoi(argv[1]);
-    int k;
+    int num_child = argc-1;
+    int i;
     int cid[num_child];
     int fd[num_child][2];
     int fdtwo[num_child][2];
     int ppid = getpid();
     int pid = ppid;
     int child_id = 0;
-    
     //prepare pipe
-    for(k=0;k<num_child;k++){
+    for(i=0;i<num_child;i++){
         if(ppid == pid){
-            if(pipe(fd[k])<0){
+            if(pipe(fd[i])<0){
                 printf("Fail to create pipe");
             }
-            if(pipe(fdtwo[k])<0){
+            if(pipe(fdtwo[i])<0){
                 printf("Fail to create pipe");
             }
         }
     }
-
     
     //Fork process
-    for(k=0;k<num_child;k++){
+    for(i=0;i<num_child;i++){
         if(ppid == pid){
-            cid[k] = fork();
+            cid[i] = fork();
             pid=getpid();
-            child_id=k+1;
+            child_id=i;
         }
     }
     pid = getpid();
     if(ppid == pid){
         //parent process
-        printf("Parent: the child players are ");
-        int u;
-        for (u=0;u<num_child;u++)
-            printf("%d ",cid[u]);
+        printf("Parent: all child id:");
+        //print child id
+        for (i=0;i<num_child;i++)
+            printf("%d ",cid[i]);
         printf("\n");
-        //call child
-        int nw;
-        for (nw=0; nw<num_child; nw++) {
-            write(fd[nw][1],"childPrint",sizeof("childPrint"));
-        }
-        int count=0;
-        for (nw=0; nw<num_child; nw++) {
-            //read command
-            char command[80];
-            read(fdtwo[nw][0], command, 80);
-            if(strcmp(command, "parentPrint")==0){
-                count++;
+        int quit=0;
+        while(quit==0){
+            char *s = (char*) malloc( 100 );
+            scanf("%s",s);
+            //call child
+            if(strstr(s, "quit") != NULL){
+                break;
             }
-        if(count==num_child)
-            printf("Parent: trump suit %c\n",card[atoi(argv[1])*atoi(argv[2])][0] );
-       }
-        printf("Parent: child %d to play first\n",handcard%num_child);
-        for (nw=0; nw<num_child; nw++) {
-            //ask to show
-            write(fd[nw][1],"childShow",sizeof("childShow"));
-            
-            //waiting children
-            char c_cardBuf[10];
-            read(fdtwo[nw][0],c_cardBuf,10);
-            printf("Parent: child %d plays %s\n",nw+1,c_cardBuf);
-        }
-        int n;
-        for (n = 0; n<num_child-1; n++) {
-            wait(NULL);
-        }
-        int x;
-        for(n=0; n<num_child-1;n++){
-            for(x=0;x<2;x++){
-                close(fd[n][x]);
-                close(fdtwo[n][x]);
+            for (i=0; i<num_child; i++) {
+                write(fd[i][1],s,100);
             }
         }
-    }else{
-        int m=child_id-1;
-        int l;
-        int cid=getpid();
+
+            }else{
+        //Child process
         while(1){
             //read command
             char command[80];
-            read(fd[child_id-1][0], command, 80);
-            if(strcmp(command, "childPrint")==0){
-                printf("Child %d, pid %d: ",child_id,cid);
-                for(l=0;l<handcard;l++)
-                {
-                    printf("%c%c ",card[m][0],card[m][1]);
-                    m=m+num_child;
-                }
-                printf("\n");
-                write(fdtwo[child_id-1][1],"parentPrint",sizeof("parentPrint"));
+            read(fd[child_id][0], command, 80);
+            if(strstr(command, "printhihi") != NULL){
+                printf("hihi,im child %d\n",pid);
             }
-            break;
-        }
-        m=child_id-1;
-        while(1){
-            //read command
-            char command[80];
-            read(fd[child_id-1][0], command, 80);
-            if(strcmp(command, "childShow")==0){
-                printf("Child %d: play ",child_id);
-                l=0;
-                    printf("%c%c ",card[m][0],card[m][1]);
-                
-                printf("\n");
-                write(fdtwo[child_id-1][1],card[m],2);
-                 m=m+num_child;
+            if(strstr(command, "printbyebye") != NULL){
+                printf("byebye,im child %d\n",pid);
             }
-            break;
         }
         
     }
